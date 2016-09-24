0245bc2485bbfe2e701344e85f34f92c21fb8c55
rustielin <rustie117@gmail.com>
2016-09-24 13:22:50 -0700

    andy pls fix pitch

--- freq_detect.py
+++ freq_detect.py
@@ -3,6 +3,21 @@ import pyaudio
 import wave
 import numpy as np
 
+from math import log2, pow
+
+A4 = 440
+C0 = A4*pow(2, -4.75)
+name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+#this takes in an int and makes string note value
+def pitch(freq):
+    # freq = int(freq)
+    h = round(12*log2(freq/C0))
+    octave = h // 12
+    n = h % 12
+    return name[n] + str(octave)
+
+
 chunk = 2048
 
 # open up a wave
@@ -37,11 +52,11 @@ while len(data) == chunk*swidth:
         y0,y1,y2 = np.log(fftData[which-1:which+2:])
         x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
         # find the frequency and output it
-        thefreq = (which+x1)*RATE/chunk
-        print("The freq is %f Hz." % (thefreq))
+        thefreq = int((which+x1)*RATE/chunk)
+        print("The freq is %s Hz." % pitch(thefreq))
     else:
-        thefreq = which*RATE/chunk
-        print("The freq is %f Hz." % (thefreq))
+        thefreq = int(which*RATE/chunk)
+        print("The freq is %s Hz." % pitch(thefreq))
     # read some more data
     data = wf.readframes(chunk)
 if data:
